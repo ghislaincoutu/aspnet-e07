ac43ae137cd26687bbcbd7ab5655e58dc171045b
ghislain.coutu <ghislain.coutu@cgi.com>
2026-06-29 14:02:35 -0400

    Exercice ASP.NET

--- related_files/Scripts/publish_aspnet.sh
+++ related_files/Scripts/publish_aspnet.sh
@@ -9,7 +9,6 @@ apply_pause "Appuyer sur la touche [Retour] pour continuer..."
 
 # Source (so)
 so=/home/dev2607/Documents/XD01/aspnet-e07/aspnet07/bin/Release/net8.0/linux-x64/publish
-
 # Destination (de)
 de=/var/www/aspnet07
 
